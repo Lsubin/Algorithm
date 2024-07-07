bab883b069920f90c5fd5ad258f2146791439e49
Lsubin <70558024+Lsubin@users.noreply.github.com>
2024-07-07 21:57:10 +0900

    20240707
    DFS&BFS

---
+++ baekjoon/DFS&BFS/24445.py
@@ -0,0 +1,33 @@
+import sys
+sys.setrecursionlimit(10**6)
+from collections import deque
+
+n, m, r = map(int,sys.stdin.readline().rstrip().split())
+
+graph = [[] for _ in range(n+1)]
+
+visited = [0]*(n+1)
+
+c = 1
+def bfs(graph, start, visited):
+    global c
+    q = deque([start])
+    visited[start] = 1
+    while q:
+        x = q.popleft()
+        graph[x].sort(reverse=True)
+        for i in graph[x]:
+            if visited[i] == 0:
+                c += 1
+                visited[i] = c
+                q.append(i)
+
+for i in range(m):
+    a, b = map(int,sys.stdin.readline().rstrip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+bfs(graph, r, visited)
+
+for i in visited[1:]:
+    print(i)
---
+++ baekjoon/DFS&BFS/24479.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+n, m, r = map(int, sys.stdin.readline().rstrip().split())
+
+graph = [[]for _ in range(n+1)]
+
+visited = [0] * (n+1)
+
+c = 1
+def dfs(graph, v, visited):
+    global c
+    visited[v] = c
+    for i in graph[v]:
+        if visited[i] == 0:
+            c += 1
+            dfs(graph,i,visited)
+
+for i in range(m):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(n+1):
+    graph[i].sort()
+
+dfs(graph,r,visited)
+
+for i in range(1,n+1):
+    print(visited[i])
